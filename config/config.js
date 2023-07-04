ce0ad1e7d2c09d6c78a1e7b21a0d3f7e8b2d286a
unknown <wtesting231@gmail.com>
2023-07-05 00:12:29 +0800

    2

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+module.exports = {
+  "development": {
+    "username": "postgres",
+    "password": "12345678",
+    "database": "database_development_tugas_akhir",
+    "host": "127.0.0.1",
+    "dialect": "postgres"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": process.env.PROD_DB_USERNAME,
+    "password": process.env.PROD_DB_PASSWORD,
+    "database": process.env.PROD_DB_NAME,
+    "host": process.env.PROD_DB_HOSTNAME,
+    "dialect": "postgres"
+  }
+}

1e8a651e223c8c9bdae3d196cf7e9052a200dc58
unknown <wtesting231@gmail.com>
2023-07-08 13:55:14 +0800

    tambah data kacamatan dan kelurahan dan perbarui form login

--- middlewere/validateSize.js
+++ middlewere/validateSize.js
@@ -3,7 +3,6 @@ module.exports = {
     validateSizeFile : (file) => {
         const message = 'Maksimal 2mb untuk Ukuran Poto '
         if(file.photoIdCard[0].size > 20715200 ) throw new Error(message + 'KTP')
-        else if (file.photoIdCard[0].mimetype !== ('image/png'|| 'image/jpg' || 'image/jpeg' )) throw new Error('Poto Harus dalam bentuk PNG | JPEG')
         if(file.photoYellowCard[0].size > 20715200) throw new Error(message + 'kartu Kuning')
         if(file.photoRTRWCertificate[0].size > 20715200) throw new Error(message + 'Surat Keterangan RT/RW')
         if(file.descriptionOfDeathInHospital) {
